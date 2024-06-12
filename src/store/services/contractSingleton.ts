985f29f39765e20a40deed49e7ab70916b87b3c0
vuonghuuhung <vuonghuuhung2002@gmail.com>
2024-06-12 15:22:51 +0700

    update

--- src/store/services/contractSingleton.ts
+++ src/store/services/contractSingleton.ts
@@ -1,10 +1,8 @@
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
 import { OraiswapV3Client } from '../../sdk'
 import { OraiswapTokenClient } from '@oraichain/oraidex-contracts-sdk'
-import { Token } from '@store/consts/static'
-import { PayloadAction } from '@reduxjs/toolkit'
-import { _newPoolKey, toSqrtPrice } from '@/wasm/oraiswap_v3_wasm'
-import { ArrayOfTupleOfUint16AndUint64, LiquidityTick, PoolKey, PoolWithPoolKey, Tick } from '@/sdk/OraiswapV3.types'
+import { _newPoolKey } from '@/wasm/oraiswap_v3_wasm'
+import { ArrayOfTupleOfUint16AndUint64, LiquidityTick, PoolKey } from '@/sdk/OraiswapV3.types'
 import {
   Tickmap,
   getLiquidityTicksLimit,
@@ -33,14 +31,7 @@ export const integerSafeCast = (value: bigint): number => {
 }
 
 export default class SingletonOraiswapV3 {
-  private static _tokens: Record<string, Token> = {}
-  private static pools: { [key in string]: PoolWithPoolKey } = {}
-  private static poolKeys: { [key in string]: PoolKey } = {}
-  private static poolTicks: { [key in string]: LiquidityTick[] } = {}
-  private static nearestPoolTicksForPair: { [key in string]: Tick[] } = {}
-  private static isLoadingLatestPoolsForTransaction: boolean = false
-  private static isLoadingTicksAndTickMaps: boolean = false
-  private static tickMaps: { [key in string]: string } = {}
+  private static _tokens: {[key: string]: OraiswapTokenClient} = {}
   private static _dex: OraiswapV3Client
 
   private constructor() {}
@@ -64,33 +55,7 @@ export default class SingletonOraiswapV3 {
     sender: string,
     contractAddress: string
   ) {
-    return new OraiswapTokenClient(signingClient, sender, contractAddress)
-  }
-
-  public static async handleInitPool(action: PayloadAction<PoolKey>): Generator {
-    try {
-      const { token_x: tokenX, token_y: tokenY, fee_tier: feeTier } = action.payload
-
-      const poolKey = _newPoolKey(tokenX, tokenY, feeTier)
-
-      const initSqrtPrice = toSqrtPrice(1n, 0n)
-
-      const tx = this._dex.createPool({
-        feeTier,
-        initSqrtPrice,
-        initTick:
-      })
-
-      const signedTx =
-        yield *
-        call([tx, tx.signAsync], walletAddress, {
-          signer: adapter.signer as Signer
-        })
-
-      const txResult = yield * call(sendTx, signedTx)
-    } catch (error) {
-      console.log(error)
-    }
+    this._tokens[contractAddress] = new OraiswapTokenClient(signingClient, sender, contractAddress)
   }
 
   public static async getRawTickmap(
