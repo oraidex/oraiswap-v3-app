3e65807c34657a7ed89e63ed5acf3e43f8e4f150
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2024-06-24 15:12:29 +0700

    add path v3

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+window.walletType = 'keplr';
+if (!window.keplr) {
+  window.keplr = window.parent.keplr;
+  window.Keplr = window.keplr;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
